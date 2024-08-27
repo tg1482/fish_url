8d07ddfebc89b6a7b8ca9ca50691c836e0111ff2
Tanmay Gupta <tg1482@stern.nyu.edu>
2024-08-27 13:07:14 +0530

    quick updates

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,36 @@
 use std::env;
-use serde_json::Value;
 use reqwest::blocking::Client;
 use regex::Regex;
 
-const CONFIG: &str = r#"
-{
-    "services": [
-        {
-            "name": "Google",
-            "url": "https://webcache.googleusercontent.com/search?q=cache:",
-            "error": "This page appears to have been removed"
-        },
-        {
-            "name": "Freedium",
-            "url": "https://freedium.cfd/",
-            "error": "Please check the URL for any typing errors."
-        },
-        {
-            "name": "Archive",
-            "url": "https://web.archive.org/web/",
-            "error": "Wayback Machine doesn't have that page archived"
-        },
-        {
-            "name": "Ghostarchive",
-            "url": "https://ghostarchive.org/search?term=",
-            "error": "No archives for that site."
-        }
-    ]
+#[derive(Debug)]
+struct Service {
+    name: &'static str,
+    url: &'static str,
+    error: &'static str,
 }
-"#;
+
+static SERVICES: &[Service] = &[
+    Service {
+        name: "Google",
+        url: "https://webcache.googleusercontent.com/search?q=cache:",
+        error: "This page appears to have been removed",
+    },
+    Service {
+        name: "Freedium",
+        url: "https://freedium.cfd/",
+        error: "Please check the URL for any typing errors.",
+    },
+    Service {
+        name: "Archive",
+        url: "https://web.archive.org/web/",
+        error: "Wayback Machine doesn't have that page archived",
+    },
+    Service {
+        name: "Ghostarchive",
+        url: "https://ghostarchive.org/search?term=",
+        error: "No archives for that site.",
+    },
+];
 
 fn check_url(client: &Client, url: &str, service: &str, error_msg: &str) -> bool {
     println!("Checking {}...", service);
@@ -82,26 +84,20 @@ fn main() {
 
     let original_url = add_www(&args[1]);
     println!("Using URL: {}", original_url);
+    println!();
 
     let client = Client::builder()
         .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
         .build()
         .unwrap();
 
-    let config: Value = serde_json::from_str(CONFIG).unwrap();
-    let services = config["services"].as_array().unwrap();
-
-    for service in services {
-        let name = service["name"].as_str().unwrap();
-        let url = service["url"].as_str().unwrap();
-        let error = service["error"].as_str().unwrap();
-
-        let cache_url = format!("{}{}", url, original_url);
-        if check_url(&client, &cache_url, name, error) {
-            println!("{} version available: {}", name, cache_url);
+    for service in SERVICES {
+        let cache_url = format!("{}{}", service.url, original_url);
+        if check_url(&client, &cache_url, &service.name, &service.error) {
+            println!("{} version available: {}", service.name, cache_url);
             std::process::exit(0);
         } else {
-            println!("{} version not available or blocked.", name);
+            println!("{} version not available or blocked.", service.name);
             println!();
         }
     }
